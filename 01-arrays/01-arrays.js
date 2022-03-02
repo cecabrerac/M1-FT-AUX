18f47d061ca5015e98534b22e9651198cfe9a621
cecabrerac <ccabrera9@hotmail.com>
2022-03-01 19:43:03 -0500

    Arrays homework done!

--- 01-arrays/01-arrays.js
+++ 01-arrays/01-arrays.js
@@ -5,15 +5,27 @@ function invertirOrden(array) {
     //
     // DETALLE: En caso de que el elemento contenga mas de 1 digito, el mismo NO debera ser devuelto
     // No vale usar el metodo "reverse"
-    
- 
+    let newArray = [];
+    for (let i=0; i < array.length; i++) {
+        if (array[i] >(-10) && array[i] <10 ) {
+            newArray.unshift(array[i]);
+        }
+    }
+    return newArray;   
 };
 
 
 function numeroEnComun(array1, array2) {
     // Entre los dos array's que recibe la funcion por parametro
     // Buscar y retornar el valor en comun entre ellos
-   
+    for (let i=0; i<array1.length; i++) {
+        for (let j=0; j<array2.length; j++) {
+            if (array1[i] === array2[j]) {
+                return array1[i];
+            }
+        }
+    }
+    return Math.min(...array1, ...array2);
 };
 
 
@@ -23,7 +35,15 @@ function sumaDeArrays(array) {
     // Tienen que devolver UN SOLO array que solo contenga elementos de tipo number
     // Sumando los elementos de cada array que contenga dos elementos, y devolviendo la suma del mismo
     // OJO: Si el elemento dentro del array que ingresa por prop, ya es de tipo number, deben devolverlo como tal dentro del array que retornan.
-   
+   let newArray = [];
+   array.forEach(element=>{
+       if(Array.isArray(element)) {
+           let suma = 0;
+           element.forEach(e=> suma+=e);
+           newArray.push(suma);
+       } else newArray.push(element);
+   })
+   return newArray;
 };
 
 
@@ -31,15 +51,29 @@ function mismoValorMismosElementos(numero, divisor) {
     // Tiene que devolver un array con la misma cantidad de elementos que el valor del divisor
     // Todos los elementos deben tener el mismo valor
     // OJO: Si el resultado de la division no es un entero, deben devolver false
-
+    let arr=[];
+    if (numero % divisor !== 0) return false;
+    else {
+        for (let i=1; i <= divisor; i++) {
+            arr.push(numero/divisor);
+        }
+    }
+    return arr;
 };
+
 
 
 function elementoMenorYMayor(array) {
     // El Array recibido por props es un array que contienen numeros
     // Tenes que retornar un array
     // Solamente con el elemento menor y mayor del array recibido
-    
+    let min = array[0];
+    let max = array[0];
+    for (let i=0; i < array.length; i++) {
+        if(array[i] < min) min = array[i];
+        if(array[i] > max) max = array[i];
+    }
+    return [min,max];
 };
 
 
