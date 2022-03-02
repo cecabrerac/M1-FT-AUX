7e4c7dffd7440c9d52199d158fba4b3e126117bc
cecabrerac <ccabrera9@hotmail.com>
2022-03-01 19:44:38 -0500

    Objects homework done!

--- 02-Objects/02-Objects.js
+++ 02-Objects/02-Objects.js
@@ -5,19 +5,21 @@ function crearClaseLibro() {
       // El constructor de la clase Libro recibe titulo (string), autor (string), traducciones (array de objetos)
       // Inicializar las propiedades del libro con los valores recibidos como argumento
       // Tu código aca:
-      
-      
+      this.titulo = titulo;
+      this.autor = autor;
+      this.traducciones = traducciones;
     }
 
     getTitulo() {
       // este método debe retornar el titulo del libro.
       // Tu código aca:
-      
+      return this.titulo;
     }
 
     getAutor() {
       // El método debe retornar nombre y apellido del autor
       // Tu código aca:
+      return this.autor;
       
     }
 
@@ -26,6 +28,7 @@ function crearClaseLibro() {
       // { idioma: idioma, editorial: editorial} al arreglo de traducciones del libro.
       // No debe retornar nada.
       // Tu código aca:
+      this.traducciones.push({idioma: idioma, editorial: editorial});
       
     }
 
@@ -35,8 +38,12 @@ function crearClaseLibro() {
       // Suponiendo que el libro tiene estas traducciones: [{idioma: 'inglés', editorial: 'Scholastic'}, {idioma: 'castellano', editorial: 'Santillana'}]
       // libro.getTraducciones() debería devolver ['inglés', 'castellano']
       // Tu código aca:
-      
-
+      let _length = this.traducciones.length;
+      let newArray = [];
+      for (let i = 0; i < _length; i++) {
+        newArray.push(this.traducciones[i].idioma);
+      }
+       return newArray;
     }
 
     getAlcance() {
@@ -46,9 +53,19 @@ function crearClaseLibro() {
       // Suponiendo que el libro tiene estas traducciones: [{idioma: 'inglés', editorial: 'Scholastic'}, {idioma: 'castellano', editorial: 'Santillana'}]
       // libro.getAlcance() deberia devolver 2
       // Tu código aca:
-      
+      return this.traducciones.length;      
 
     }
+
+    printStaff(hogwarts) {
+      let a = ('The headmaster is '+ this.staff.headmaster.name);
+      let b = ('The keeperOfKeys is '+ this.staff.keeperOfKeys.name);
+      let c = ('The potionsMaster is '+ this.staff.potionsMaster.name);
+      let d = ('The headOfGryffindor is '+ this.staff.headOfGryffindor.name);
+      return ([a,b,c,d]);
+    }
+
+
   }
 
   return Libro;
