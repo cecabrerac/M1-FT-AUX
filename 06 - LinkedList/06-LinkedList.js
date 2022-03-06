ff9a1498eecdd2ca99fa0d1468a3610c4e9bba70
cecabrerac <ccabrera9@hotmail.com>
2022-03-06 09:14:12 -0500

    linkedList done!

--- 06 - LinkedList/06-LinkedList.js
+++ 06 - LinkedList/06-LinkedList.js
@@ -2,9 +2,15 @@ const { LinkedList, Node } = require("../estructuras")
   //agregar el metodo size al prototipo de LinkedList.
   // Este metodo deberia retornar la cantidad de elementos de la lista
   LinkedList.prototype.size=function(){
-      
+      let count = 0;
+      if (this.head === null) return 0;
+      current = this.head;
+      while(current) {
+        count++
+        current = current.next;
+      }
+      return count;
   }
-
 
 // Agregar el método orderList al prototipo de LinkedList. Este método deberá ordenar los elementos de nuestra lista enlazada de mayor a menor.
 // Ejemplo:
@@ -14,7 +20,21 @@ const { LinkedList, Node } = require("../estructuras")
 // ACLARACIÓN: Se debe ordenar la lista original y no una nueva.
 LinkedList.prototype.orderList = function () {
     // Tu código aca:
-    
+    if(this.head === null) return null;
+    let current1 = this.head;
+    while (current1) {
+      let current2 = current1.next
+      while (current2){
+        if(current1.value < current2.value) { // si current1 es menor que current2 swap them
+          let aux= current2.value;
+          current2.value = current1.value;
+          current1.value = aux;
+        }
+        current2 = current2.next;
+      }
+      current1 = current1.next;
+    }
+    return this;
   };
 
 
@@ -25,7 +45,27 @@ LinkedList.prototype.orderList = function () {
    
   // 1-->2-->3-->2.5-->4   2.5 pos=1  newnodo= 2.5-->4   curr=3   
 LinkedList.prototype.insert = function(data,pos) {
-	
+	if (pos >= this.size()) return 'pos excede tamaño de la lista';
+  else if (pos === 0) {
+    newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+    
+    else {
+      let count = 1;
+      current = this.head;
+      while (current) {
+        if(count === pos) {
+          newNode = new Node(data);
+          newNode.next = current.next;
+          current.next = newNode;
+        }
+        count++;
+        current = current.next;
+      }
+    }
+
 }
   
 
